f6de1a0a92e7dc350bcd4a1e4060854b8906a5af
Krishna Kant Singh <kks1102000@gmail.com>
2019-06-11 00:17:57 +0530

    Revert "added user and settings"

--- Desktop/squiz-master/squiz/quiz/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Test, Question, Options, Score, Response
-
-
-admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(Options)
-admin.site.register(Score)
-admin.site.register(Response)
